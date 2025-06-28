04eca81987ca079dd73dbaf620fb37554bcd7a63
HSE-ST <hset.mbma@sinarterangmandiri.com>
2025-06-28 15:26:06 +0800

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ from datetime import datetime
 st.set_page_config(page_title="Personal Safety Discussion", page_icon="🛡️", layout="wide")
 
 # Spreadsheet credentials from Streamlit Secrets
-creds_dict = st.secrets["GOOGLE_CREDENTIALS"]
+import json
+creds_dict = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
 scope = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
